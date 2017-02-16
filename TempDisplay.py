c94a485fca7f3531f55b2a48f15b52ae8d4b571d
Jeff Olson <olsonredbull@yahoo.com>
2017-02-15 22:33:48 -0600

    added getTemp method to read on board temp chip

--- TempDisplay.py
+++ TempDisplay.py
@@ -11,6 +11,9 @@ from datetime import datetime
 import time
 from papirus import Papirus
 from papirus import PapirusImage
+import Adafruit_ADS1x15
+
+adc = Adafruit_ADS1x15.ADS1115()
 
 user = os.getuid()
 if user != 0:
@@ -28,9 +31,12 @@ TIME_FONT = '/usr/share/fonts/truetype/freefont/FreeSansBold.ttf'
 tempFont_SIZE = 70
 descriptionFont_SIZE  = 25
 timeFont_Size = 15
-
+GAIN =1
 MAX_START = 0xffff
-
+tempFont = ImageFont.truetype(FONT_FILE, tempFont_SIZE )
+descriptionFont = ImageFont.truetype(FONT_BOLD, descriptionFont_SIZE)
+timeFont = ImageFont.truetype(TIME_FONT , timeFont_Size)
+   
  
 def main(argv):
 
@@ -40,26 +46,47 @@ def main(argv):
   
 def tempDisplay(papirus):
    count = 0
-   image = Image.new('1', papirus.size, WHITE)
-
+   previous_second = 0
+   
    while (count < 60):
-        width, height = image.size
         now = datetime.today()
-        tempFont = ImageFont.truetype(FONT_FILE, tempFont_SIZE )
-        descriptionFont = ImageFont.truetype(FONT_BOLD, descriptionFont_SIZE)
-        timeFont = ImageFont.truetype(TIME_FONT , timeFont_Size)
-        img = Image.open('/home/pi/Mariah/image/coldDisp.bmp', 'r')
-        draw = ImageDraw.Draw(img)
-      #  draw.text((0,0), '{h:02d}:{m:02d}:{s:02d}'.format(h=now.hour, m=now.minute, s=now.second), fill=BLACK, font=timeFont)
-        draw.text((57,30), "88\xb0", font=tempFont)
-        draw.text((125,0),"Water",font=descriptionFont)
-        bw = img.convert("1", dither=Image.FLOYDSTEINBERG)
-        papirus.display(bw)
-        papirus.partial_update()
-        count +=1
-       
-      
+        time.sleep(0.5)
+        temp = getTemp(0)    
+        loadImage(papirus, temp)
+        if now.second < previous_second:
+           papirus.update()    # full update every minute
+            
+        else:
+            papirus.partial_update()
+        previous_second = now.second
 
+        count +=1
+
+        print('| {0} | {1} '.format(now.second,previous_second))
+
+
+
+def loadImage(papirus,temp):
+       image = Image.new('1', papirus.size, WHITE) 
+       width, height = image.size
+       img = Image.open('/home/pi/Mariah/image/coldDisp.bmp', 'r')
+       draw = ImageDraw.Draw(img)
+       draw.text((57,30), "{0}\xb0".format(temp), font=tempFont)
+       draw.text((125,0),"Air",font=descriptionFont)
+       bw = img.convert("1", dither=Image.FLOYDSTEINBERG)
+       papirus.display(bw)
+
+def getTemp(channel):
+        GAIN = 2
+        adc.start_adc(0, gain=GAIN)
+        value= adc.read_adc(channel, gain=GAIN)
+        compTemp =0
+        rawValue = adc.get_last_result()
+        compTemp = rawValue >> 5
+        value = compTemp & 0b00000000001
+        convertedTemp = compTemp * .125 * 1.8 +32
+        return int(convertedTemp)
+ 
 # main
 if "__main__" == __name__:
     if len(sys.argv) < 1:
